0aa761e0cd3f4482008c8ac0fef6e4e2314a07dd
joramuns <joramuns@student.21-school.ru>
2023-05-16 22:24:39 +0300

    feat(list): ctors, dtor, front, back, pops and pushes

--- src/list/s21_list.h
+++ src/list/s21_list.h
@@ -19,18 +19,39 @@ class list {
   using size_type = std::size_t;
 
   /*** List functions ***/
-  list();
-  explicit list(size_type n);
-  explicit list(std::initializer_list<value_type> const &items);
+  list() : head_(new node<value_type>), tail_(head_) { push_back(0); };
+
+  explicit list(size_type n) : head_(new node<T>), tail_(head_) {
+    while (--n > 0U) {
+      push_back(0);
+    }
+  };
+
+  explicit list(std::initializer_list<value_type> const &items)
+      : head_(new node<value_type>), tail_(head_) {
+    for (auto &item : items) push_back(item);
+    pop_front();
+  };
+
   list(const list &l);
+
   list(list &&l);
-  ~list();
-  list<T> operator=(const list<T> &l);
-  list<T> operator=(list &&l);
+
+  ~list() {
+    while (head_ != tail_) {
+      pop_back();
+    }
+    delete head_;
+  };
+
+  list<value_type> operator=(const list<T> &l);
+
+  list<value_type> operator=(list &&l);
 
   /*** List element access ***/
-  const_reference front();
-  const_reference back();
+  const_reference front() { return head_->value_; };
+
+  const_reference back() { return tail_->value_; };
 
   /*** List iterators ***/
   iterator begin();
@@ -45,10 +66,30 @@ class list {
   void clear();
   iterator insert(iterator pos, const_reference value);
   void erase(iterator pos);
-  void push_back(const_reference value);
-  void pop_back();
-  void push_front(const_reference value);
-  void pop_front();
+  void push_back(const_reference value) {
+    node<value_type> *temp = new node<value_type>;
+    temp->prev_ = tail_;
+    tail_->next_ = temp;
+    temp->value_ = value;
+    tail_ = temp;
+  };
+  void pop_back() {
+    node<value_type> *temp = tail_->prev_;
+    delete tail_;
+    tail_ = temp;
+  }
+  void push_front(const_reference value) {
+    node<value_type> *temp = new node<value_type>;
+    temp->next_ = head_;
+    head_->prev_ = temp;
+    temp->value_ = value;
+    head_ = temp;
+  };
+  void pop_front() {
+    node<value_type> *temp = head_->next_;
+    delete head_;
+    head_ = temp;
+  };
   void swap(list &other);
   void merge(list &other);
   void splice(const_iterator pos, list &other);
@@ -57,9 +98,15 @@ class list {
   void sort();
 
  private:
-  list<T> *next_;
-  list<T> *prev_;
-  value_type value_;
+  template <typename>
+  struct node {
+    node<value_type> *next_ = nullptr;
+    node<value_type> *prev_ = nullptr;
+    T data_ = 0;
+    value_type value_;
+  };
+  node<value_type> *head_;
+  node<value_type> *tail_;
 };
 }  // namespace s21
 
