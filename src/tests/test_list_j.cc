3f76377e4dd33f6adefd61e7a218ab394562717d
joramuns <joramuns@student.21-school.ru>
2023-05-22 21:34:00 +0300

    tests(list): size, empty tests

--- src/tests/test_list_j.cc
+++ src/tests/test_list_j.cc
@@ -36,3 +36,63 @@ TEST(List, Filling_char_1) {
   ASSERT_EQ(aboba.front(), 'q');
   ASSERT_EQ(aboba.back(), '[');
 }
+
+TEST(List, Size_1) {
+  s21::list<int> aboba(5);
+  ASSERT_EQ(aboba.size(), 5);
+}
+
+TEST(List, Size_2) {
+  s21::list<int> aboba(0);
+  ASSERT_EQ(aboba.size(), 0);
+}
+
+TEST(List, Size_3) {
+  s21::list<int> aboba;
+  aboba.pop_back();
+  aboba.push_back(1);
+  aboba.pop_front();
+  aboba.pop_back();
+  aboba.pop_front();
+  aboba.pop_front();
+  aboba.pop_front();
+  aboba.push_front(99);
+  ASSERT_EQ(aboba.size(), 1);
+}
+
+TEST(List, Empty_1) {
+  s21::list<int> aboba(9);
+  aboba.push_back(13);
+  aboba.push_back(99);
+  for (int i = 0; i < 11; i++) {
+    aboba.pop_back();
+  }
+  ASSERT_EQ(aboba.empty(), true);
+}
+
+TEST(List, Empty_2) {
+  s21::list<int> aboba(9);
+  aboba.push_back(13);
+  aboba.push_back(99);
+  for (int i = 0; i < 11; i++) {
+    aboba.pop_front();
+  }
+  ASSERT_EQ(aboba.empty(), true);
+}
+
+TEST(List, Empty_3) {
+  s21::list<int> aboba;
+  aboba.pop_back();
+  aboba.pop_front();
+  ASSERT_EQ(aboba.empty(), true);
+}
+
+TEST(List, Empty_4) {
+  s21::list<int> aboba;
+  aboba.pop_back();
+  aboba.push_back(1);
+  aboba.pop_front();
+  aboba.pop_back();
+  aboba.pop_front();
+  ASSERT_EQ(aboba.empty(), true);
+}
