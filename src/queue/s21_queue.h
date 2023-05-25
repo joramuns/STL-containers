652a026377e74f17c1a9683b1ffc186676263dc5
joramuns <joramuns@student.21-school.ru>
2023-05-25 20:58:43 +0300

    feat(queue): all functions ready, not tested

--- src/queue/s21_queue.h
+++ src/queue/s21_queue.h
@@ -15,28 +15,49 @@ class queue {
   using size_type = std::size_t;
 
   /*** Queue member functions ***/
-  queue();
-  explicit queue(std::initializer_list<value_type> const &items);
-  queue(const queue &q);
-  queue(queue &&q);
+  queue(){};
+
+  explicit queue(std::initializer_list<value_type> const &items)
+      : data_(items){};
+
+  queue(const queue &q) : data_(q.data_){};
+
+  queue(queue &&q) { std::swap(data_, q.data_); };
+
   ~queue();
-  queue &operator=(const queue &q);
-  queue &operator=(queue &&q);
+
+  queue &operator=(const queue &q) {
+    data_ = q.data_;
+
+    return *this;
+  };
+
+  queue &operator=(queue &&q) {
+    std::swap(data_, q.data_);
+
+    return *this;
+  };
 
   /*** Queue element access ***/
-  const_reference front() const noexcept;
-  const_reference back() const noexcept;
-  reference front() noexcept;
-  reference back() noexcept;
+  const_reference front() const noexcept { return data_.front(); };
+
+  const_reference back() const noexcept { return data_.back(); };
+
+  reference front() noexcept { return data_.front(); };
+
+  reference back() noexcept { return data_.back(); };
 
   /*** Queue capacity ***/
-  bool empty() const noexcept;
-  size_type size() const noexcept;
+  bool empty() const noexcept { return data_.empty(); };
+
+  size_type size() const noexcept { return data_.size(); };
 
   /*** Queue modifiers ***/
-  void push(const_reference value);
-  void pop();
-  void swap(queue &other) noexcept;
+  void push(const_reference value) { data_.push_back(value); };
+
+  void pop() { data_.pop_front(); };
+
+  void swap(queue &other) noexcept { std::swap(data_, other.data_); };
 
  private:
   Container data_;
