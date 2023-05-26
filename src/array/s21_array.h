0f18dd81d6a6e8bb3dc9844a8fe2a3220a02b15c
joramuns <joramuns@student.21-school.ru>
2023-05-26 17:30:02 +0300

    feat(array): ctors, assignment operators, front, back, data

--- src/array/s21_array.h
+++ src/array/s21_array.h
@@ -26,15 +26,27 @@ class array {
     for (const auto& item : items) data_[counter++] = item;
   };
 
-  array(const array &a);
+  array(const array &a) {
+    size_type a_size = a.size();
+    for (size_type i = 0; i != a_size; ++i) data_[i] = a.data_[i]; 
+  };
 
-  array(array &&a) noexcept;
+  array(array &&a) noexcept { std::swap(data_, a.data_); };
 
   ~array() noexcept {};
 
-  array &operator=(const array &a);
+  array &operator=(const array &a) {
+    size_type a_size = a.size();
+    for (size_type i = 0; i != a_size; ++i) data_[i] = a.data_[i]; 
+    
+    return *this;
+  };
 
-  array &operator=(array &&a) noexcept;
+  array &operator=(array &&a) noexcept {
+    std::swap(data_, a.data_);
+
+    return *this;
+  };
 
   /*** Array element access ***/
   reference at(size_type pos) {
@@ -47,11 +59,27 @@ class array {
     return at(pos);
   };
 
-  const_reference front() const noexcept;
-  const_reference back() const noexcept;
-  reference front() noexcept;
-  reference back() noexcept;
-  iterator data() const noexcept;
+  const_reference at(size_type pos) const {
+    if (pos > N - 1)
+      throw std::invalid_argument("Index is out of range");
+    return data_[pos];
+  };
+
+  const_reference operator[](size_type pos) const {
+    return at(pos);
+  };
+
+  const_reference front() const noexcept { return data_[0]; };
+
+  const_reference back() const noexcept { return data_[size() - 1]; };
+
+  reference front() noexcept { return data_[0]; };
+
+  reference back() noexcept { return data_[size() - 1]; };
+  
+  const_iterator data() const noexcept { return data_; };
+
+  iterator data() noexcept { return data_; };
   
   /*** Array iterators ***/
   iterator begin() noexcept { return data_; };
@@ -80,4 +108,5 @@ class array {
   value_type data_[N] = {};
 };
 }  // namespace s21
+ 
 #endif  // CPP2_S21_CONTAINERS_0_ARRAY_S21_ARRAY_H_
