4c027e9408af3f4e04f8c30478fbc1c6f0fc672a
joramuns <joramuns@student.21-school.ru>
2023-05-26 16:56:25 +0300

    feat(array): init ctor, at, operator[], begin, end, empty, size, swap, fill

--- src/array/s21_array.h
+++ src/array/s21_array.h
@@ -2,6 +2,8 @@
 #define CPP2_S21_CONTAINERS_0_ARRAY_S21_ARRAY_H_
 
 #include <initializer_list>
+#include <algorithm>
+#include <iostream>
 
 namespace s21 {
 template <typename T, std::size_t N>
@@ -15,17 +17,36 @@ class array {
   using size_type = std::size_t;
 
   /*** Array member functions ***/
-  array();
-  explicit array(std::initializer_list<value_type> const &items);
+  array() noexcept {};
+
+  explicit array(std::initializer_list<value_type> const &items) {
+    if (items.size() != N)
+      throw std::invalid_argument("Size of container and number of elements are not equal");
+    size_type counter = 0;
+    for (const auto& item : items) data_[counter++] = item;
+  };
+
   array(const array &a);
+
   array(array &&a) noexcept;
-  ~array() noexcept;
+
+  ~array() noexcept {};
+
   array &operator=(const array &a);
+
   array &operator=(array &&a) noexcept;
 
   /*** Array element access ***/
-  reference at(size_type pos);
-  reference operator[](size_type pos);
+  reference at(size_type pos) {
+    if (pos > N - 1)
+      throw std::invalid_argument("Index is out of range");
+    return data_[pos];
+  };
+
+  reference operator[](size_type pos) {
+    return at(pos);
+  };
+
   const_reference front() const noexcept;
   const_reference back() const noexcept;
   reference front() noexcept;
@@ -33,20 +54,30 @@ class array {
   iterator data() const noexcept;
   
   /*** Array iterators ***/
-  iterator begin() const noexcept;
-  iterator end() const noexcept;
+  iterator begin() noexcept { return data_; };
+
+  iterator end() noexcept { return data_ + N; };
+  
+  const_iterator begin() const noexcept { return data_; };
+
+  const_iterator end() const noexcept { return data_ + N; };
 
   /*** Array capacity ***/
-  bool empty() const noexcept;
-  size_type size() const noexcept;
+  bool empty() const noexcept { return begin() == end(); };
+
+  size_type size() const noexcept { return end() - begin(); };
+
   size_type max_size() const noexcept;
 
   /*** Array modifiers ***/
-  void swap(array &other) noexcept;
-  void fill(const_reference value) noexcept;
+  void swap(array &other) noexcept { 
+    std::swap(data_, other.data_);
+  };
+
+  void fill(const_reference value) noexcept { std::fill(begin(), end(), value); };
 
  private:
-  value_type *data_;
+  value_type data_[N] = {};
 };
 }  // namespace s21
 #endif  // CPP2_S21_CONTAINERS_0_ARRAY_S21_ARRAY_H_
