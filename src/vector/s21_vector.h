f1b02b6783a1e460fba48792c8b7be17f48405c5
Mammie Mikken <mammiemi@ga-x2.msk.21-school.ru>
2023-05-28 15:36:45 +0300

    feat:change vector construct for initializer_list

--- src/vector/s21_vector.h
+++ src/vector/s21_vector.h
@@ -9,51 +9,64 @@ template <typename T> class vector {
 public:
   using value_type = T;
   using reference = T &;
-  using const_reference = const reference;
+  using const_reference = const T &;
   using iterator = T *;
-  using const_iterator = const iterator;
+  using const_iterator = const T *;
   using size_type = std::size_t;
 
   // vector member function
 
-  vector() : sz_(1), cpct_(1) { InitVector(); };
+  vector() : sz_(0), cpct_(0){};
 
   explicit vector(size_type n) : sz_(n), cpct_(n) {
     InitVector();
-    while (n--) {
-      push_back(0);
+    for (size_type i = 0; i < n; ++i) {
+      arr_[i] = 0;
     }
   };
 
-  explicit vector(std::initializer_list<value_type> const &items) {
+  explicit vector(std::initializer_list<value_type> const &items)
+      : sz_(0), cpct_(0) {
+    InitVector();
     for (const auto &item : items) {
       push_back(item);
     }
   };
 
-  vector(const vector &v){
-		sz_ = v.sz_;
-		cpct_ = v.cpct_;
-		InitVector();
-		CopyArr(v);
+  vector(const vector &v) {
+    sz_ = v.sz_;
+    cpct_ = v.cpct_;
+    InitVector();
+    CopyArr(v);
   };
 
   vector(vector &&v) : vector() {
-		std::swap(sz_, v.sz_);
-		std::swap(cpct_, v.cpct_);
-		InitVector();
-		std::swap(arr_, v.arr_);
+    std::swap(sz_, v.sz_);
+    std::swap(cpct_, v.cpct_);
+    InitVector();
+    std::swap(arr_, v.arr_);
   };
 
   ~vector() {
     delete[] arr_;
     sz_ = 0;
     cpct_ = 0;
-		arr_ = nullptr;
+    arr_ = nullptr;
   };
 
-  operator=(vector &&v) noexcept {
+  vector operator=(vector &&v) noexcept {
+    if (this != &v) {
+      delete[] arr_;
 
+      sz_ = v.sz_;
+      cpct_ = v.cpct_;
+      CopyArr(v);
+
+      v.sz_ = 0;
+      v.cpct_ = 0;
+      v.arr_ = nullptr;
+    }
+    return *this;
   };
 
   // vector Elements access
@@ -61,53 +74,37 @@ public:
 
   };
 
-  reference operator[](size_type pos){
+  reference operator[](size_type pos) { return arr_[pos]; };
 
-  };
+  const_reference front() const { return arr_[0]; };
 
-  const_reference front() const {
-		return arr_[0];
-  };
-
-  const_reference back() const {
-		return arr_[sz_ - 1];
-  };
+  const_reference back() const { return arr_[sz_ - 1]; };
 
   T *data() noexcept {
 
   };
 
   // vector iterators
-  iterator begin() noexcept {
-		return arr_;
-  };
+  iterator begin() noexcept { return arr_; };
 
   iterator end() noexcept {
-		iterator tmp = arr_;
-		return (tmp + sz_);
+    iterator tmp = arr_;
+    return (tmp + sz_);
   };
 
   // vector capacity
 
-  bool empty() const noexcept {
-		return sz_ == 0 ? 1 : 0;
-  };
+  bool empty() const noexcept { return sz_ == 0 ? 1 : 0; };
 
-  size_type size() const noexcept {
-		return sz_;
-  };
+  size_type size() const noexcept { return sz_; };
 
-  size_type max_size() const noexcept {
-
-  };
+  size_type max_size() const noexcept { return 0; };
 
   void reserve(size_type size){
 
   };
 
-  size_type capacity() const noexcept {
-		return cpct_;
-  };
+  size_type capacity() const noexcept { return cpct_; };
 
   void shrink_to_fit(){
 
@@ -119,19 +116,19 @@ public:
 
   };
 
-  iterator insert(iterator pos, const_reference value){
-		*(++pos) = value;
-		return pos;
+  iterator insert(iterator pos, const_reference value) {
+    *(++pos) = value;
+    return pos;
   };
 
-  void erase(iterator pos){
+  // void erase(iterator pos){
 
-  };
+  // };
 
   void push_back(const_reference value) {
     ++sz_;
-    if (sz_ == cpct_) {
-      vector tmp = this;
+    if (sz_ >= cpct_) {
+      vector tmp = *this;
       this->~vector();
       sz_ = tmp.sz_;
       cpct_ = 2 * sz_;
@@ -141,9 +138,7 @@ public:
     arr_[sz_ - 1] = value;
   };
 
-  void pop_back(){
-		erase(end());
-  };
+  void pop_back() { erase(end()); };
 
   void swap(vector &other) noexcept {
 
@@ -154,7 +149,7 @@ private:
   size_type sz_;
   size_type cpct_;
 
-  void InitVector() { arr = new T[cpct_](); };
+  void InitVector() { arr_ = new T[cpct_](); };
 
   void CopyArr(const vector &other) noexcept {
     for (size_type i = 0; i < other.sz_; ++i) {
