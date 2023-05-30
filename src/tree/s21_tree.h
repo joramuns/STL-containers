7705d8a88e08f30ae3a78f317b0581587ba0d278
joramuns <joramuns@student.21-school.ru>
2023-05-30 19:13:49 +0300

    feat(tree): findnode, insertnode through findnode, iterator, const_iterator

--- src/tree/s21_tree.h
+++ src/tree/s21_tree.h
@@ -5,20 +5,26 @@
 #include <iostream>
 
 // Temp for print
-#include <string>
-#include <vector>
+#include <cmath>
 #include <iomanip>
 #include <ios>
-#include <cmath>
+#include <string>
+#include <vector>
 
 namespace s21 {
 template <typename T>
 class Tree {
+  struct TNode;
+  class TreeIterator;
+  class ConstTreeIterator;
+
  public:
   using value_type = T;
   using reference = T &;
   using const_reference = const T &;
   using size_type = std::size_t;
+  using iterator = TreeIterator;
+  using const_iterator = ConstTreeIterator;
 
   Tree(){};
 
@@ -39,29 +45,50 @@ class Tree {
 
   Tree &operator=(Tree &&other) { return *this; };
 
-  ~Tree() {
-    head_->DeleteNode(head_);
-  };
+  ~Tree() { head_->DeleteNode(head_); };
 
-  void InsertNode(value_type value) {
+  std::pair<iterator, bool> InsertNode(value_type value) {
+    std::pair<iterator, bool> result;
     if (head_) {
-      TNode *parent = head_;
-      TNode *child = (value > parent->data_) ? parent->right_ : parent->left_;
-      while (child) {
-        parent = child;
-        child = (value > parent->data_) ? parent->right_ : parent->left_;
-      }
-      child = new TNode(parent, value);
-      if (value > parent->data_) {
-        parent->right_ = child;
-      } else if (value < parent->data_) {
-        parent->left_ = child;
-      } else {
-        std::cout << "Equal nodes, temporary block" << std::endl;
+      TNode *search = FindNode(value).iter_;
+      if (value > search->data_) {
+        search->right_ = new TNode(search, value);
+        result.first = iterator(search->right_);
+        result.second = true;
+        std::cout << "inside: " << result.second << " what: " << *result.first << std::endl;
+      } else if (value < search->data_) {
+        search->left_ = new TNode(search, value);
+        result.first = iterator(search->left_);
+        result.second = true;
       }
     } else {
       head_ = new TNode(value);
+      head_->color_ = kBlack;
+      result.first = iterator(head_);
+      result.second = false;
     }
+
+    return result;
+  }
+
+  iterator FindNode(value_type value) {
+    TNode *parent = head_;
+    TNode *child = parent;
+    bool not_found = true;
+    while (child && not_found) {
+      parent = child;
+      if (value > parent->data_) {
+        child = parent->right_;
+      } else if (value < parent->data_) {
+        child = parent->left_;
+      } else {
+        not_found = false;
+      }
+    }
+    if (not_found) {
+    }
+
+    return iterator(parent);
   }
 
   void PrintTree() {
@@ -82,21 +109,65 @@ class Tree {
   };
 
  private:
+  class TreeIterator {
+   public:
+    TreeIterator(){};
+    explicit TreeIterator(TNode *tree_node) noexcept : iter_(tree_node){};
+    TreeIterator(const TreeIterator &other) noexcept : iter_(other.iter_){};
+    TreeIterator(TreeIterator &&other) noexcept { std::swap(iter_, other.iter_); }
+
+    TreeIterator &operator=(const TreeIterator &other) noexcept {
+      if (this != &other) iter_ = other.iter_;
+
+      return *this;
+    }
+    TreeIterator &operator=(TreeIterator &&other) noexcept {
+      if (this != &other) std::swap(iter_, other.iter_);
+
+      return *this;
+    }
+    operator bool() noexcept { return (iter_); }
+    operator bool() const noexcept { return (iter_); }
+    const_reference operator*() const { return iter_->data_; }
+    reference operator*() { return iter_->data_; }
+
+    TNode *iter_;
+  };
+
+  class ConstTreeIterator {
+   public:
+    ConstTreeIterator(){};
+    explicit ConstTreeIterator(const iterator &other) noexcept
+        : iter_(other.iter_){};
+
+    operator bool() noexcept { return (iter_); }
+    const_reference operator*() const { return iter_->data_; }
+
+    TNode *iter_;
+  };
+
   enum TColor { kBlack, kRed };
   struct TNode {
     TNode *parent_ = nullptr;
     TNode *left_ = nullptr;
     TNode *right_ = nullptr;
     value_type data_;
-    bool color_ = kBlack;
+    bool color_ = kRed;
 
     explicit TNode(const_reference value) : data_(value){};
-    TNode(TNode *parent, const_reference value) : parent_(parent), data_(value) {};
+    TNode(TNode *parent, const_reference value)
+        : parent_(parent), data_(value){};
 
-    size_type PrintTree(TNode *pivot, size_type h, std::vector<std::vector<std::string>> &output) {
+    size_type PrintTree(TNode *pivot, size_type h,
+                        std::vector<std::vector<std::string>> &output) {
       if (output.size() < h) output.push_back({});
       if (pivot) {
         output[h - 1].push_back(std::to_string(pivot->data_));
+        if (pivot->color_ == kBlack) {
+          output[h - 1].back() += "B";
+        } else {
+          output[h - 1].back() += "R";
+        }
         PrintTree(pivot->left_, h + 1, output);
         PrintTree(pivot->right_, h + 1, output);
       } else {
@@ -104,7 +175,7 @@ class Tree {
       }
 
       return h;
-    }; 
+    };
 
     void DeleteNode(TNode *pivot) {
       if (pivot) {
@@ -113,7 +184,6 @@ class Tree {
         delete pivot;
       }
     }
-      
   };
   TNode *head_ = nullptr;
 };
