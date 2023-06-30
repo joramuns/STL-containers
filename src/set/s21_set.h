eb57aed5fa12092e68a397da2c1928576d329760
joramuns <joramuns@student.21-school.ru>
2023-06-30 15:04:32 +0300

    feat(set): dynamically allocated member of class, swap method

--- src/set/s21_set.h
+++ src/set/s21_set.h
@@ -26,53 +26,53 @@ class set {
   explicit set(std::initializer_list<value_type> const &items){};
   set(const set &s) = default;
   set(set &&s) = default;
-  ~set(){};
+  ~set(){ delete rb_tree_; };
   /* set &operator=(const set &s){ return 0; }; */
   /* set &operator=(set &&s){ return 0; }; */
 
   /* Set iterators */
-  iterator begin() noexcept { return rb_tree_.Begin(); };
-  iterator end() noexcept { return rb_tree_.End(); };
+  iterator begin() noexcept { return rb_tree_->Begin(); };
+  iterator end() noexcept { return rb_tree_->End(); };
   /* const_iterator begin() const noexcept; */
   /* const_iterator end() const noexcept; */
-  const iterator begin() const noexcept { return rb_tree_.Begin(); };
-  const iterator end() const noexcept { return rb_tree_.End(); };
+  const iterator begin() const noexcept { return rb_tree_->Begin(); };
+  const iterator end() const noexcept { return rb_tree_->End(); };
 
   /* Set capacity */
-  bool empty() const noexcept { return rb_tree_.Empty(); };
-  size_type size() const noexcept { return rb_tree_.GetSize(); };
+  bool empty() const noexcept { return rb_tree_->Empty(); };
+  size_type size() const noexcept { return rb_tree_->GetSize(); };
   size_type max_size() const noexcept;
 
   /* Set modifiers */
-  void clear();
+  void clear() { rb_tree_->ClearTree(); };
 
   std::pair<iterator, bool> insert(const value_type &value) {
-    return rb_tree_.InsertNode(value);
+    return rb_tree_->InsertNode(value);
   };
 
-  void erase(iterator pos) { rb_tree_.DeleteNode(pos); };
+  void erase(iterator pos) { rb_tree_->DeleteNode(pos); };
 
-  void swap(set &other);
+  void swap(set &other) {
+    std::swap(rb_tree_, other.rb_tree_);
+  };
+
   void merge(set &other) {
     iterator iter = other.begin();
     iterator iter_end = other.end();
-    std::cout << "Merge here: " << std::endl;
     while (iter != iter_end) {
       if (!contains(*iter)) {
         iterator temp = iter;
         ++iter;
-        std::cout << *temp << " iter " << *iter << std::endl;
-        rb_tree_.InsertNode(other.rb_tree_.ExtractNode(temp));
+        rb_tree_->InsertNode(other.rb_tree_->ExtractNode(temp));
       } else {
         ++iter;
       }
     }
-    std::cout << "Merge out" << std::endl;
   };
 
   /* Set lookup */
   iterator find(const key_type &key) const noexcept {
-    return rb_tree_.FindKey(key);
+    return rb_tree_->FindKey(key);
   };
 
   bool contains(const key_type &key) const noexcept {
@@ -80,7 +80,7 @@ class set {
   };
 
  private:
-  tree_type rb_tree_;
+  tree_type *rb_tree_ = new tree_type;
 };
 }  // namespace s21
 
