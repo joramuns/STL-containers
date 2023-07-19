04eabfa72a7f3253bade4ced676189dcfc5cac47
joramuns <joramuns@student.21-school.ru>
2023-07-19 20:46:15 +0300

    feat(map): header structure

--- src/map/s21_map.h
+++ src/map/s21_map.h
@@ -1,4 +1,63 @@
 #ifndef CPP2_S21_CONTAINERS_0_MAP_S21_MAP_H_
 #define CPP2_S21_CONTAINERS_0_MAP_S21_MAP_H_
 
+#include "../tree/s21_tree.h"
+
+namespace s21 {
+template <typename Key, typename T>
+class map {
+ public:
+  using key_type = Key;
+  using mapped_type = T;
+  using value_type = std::pair<const key_type, mapped_type>;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using tree_type = Tree<Key, T>;
+  using iterator = typename tree_type::TreeIterator;
+  using const_iterator = typename tree_type::ConstTreeIterator;
+  using size_type = std::size_t;
+
+  /*** Map member functions ***/
+  map(){};
+  explicit map(std::initializer_list<value_type> const &items){};
+  map(const map &m){};
+  map(map &&m){};
+  ~map();
+
+  map &operator=(const map &m);
+  map &operator=(map &&m);
+
+  /*** Map element access ***/
+  mapped_type &at(const Key &key);
+  mapped_type &operator[](const Key &key);
+
+  /*** Map iterators ***/
+  iterator begin() noexcept;
+  iterator end() noexcept;
+  const_iterator begin() const noexcept;
+  const_iterator end() const noexcept;
+
+  /*** Map capacity ***/
+  bool empty();
+  size_type size();
+  size_type max_size();
+
+  /*** Map modifiers ***/
+  void clear();
+  std::pair<iterator, bool> insert(const value_type &value);
+  std::pair<iterator, bool> insert(const key_type &key, const mapped_type &obj);
+  std::pair<iterator, bool> insert_or_assign(const key_type &key,
+                                             const mapped_type &obj);
+  void erase(iterator pos);
+  void swap(map &other);
+  void merge(map &other);
+
+  /*** Map lookup ***/
+  bool contains(const key_type &key);
+
+ private:
+  tree_type *rb_tree_ = new tree_type;
+};
+}  // namespace s21
+
 #endif  // CPP2_S21_CONTAINERS_0_MAP_S21_MAP_H_
