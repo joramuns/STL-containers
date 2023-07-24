0f5ce73371b4be558d4b410944f7444061b1a779
joramuns <joramuns@student.21-school.ru>
2023-07-24 23:40:51 +0300

    tests(stack): nandaug, bezlant, arzzezzan

---
+++ src/tests/test_stack/test_stack_arzzezzan.cc
@@ -0,0 +1,290 @@
+#include "../tests.h"
+
+TEST(ArzzezzanStackTest, Constructor_default) {
+  s21::stack<int> s21_stack;
+  std::stack<int> std_stack;
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+}
+
+TEST(ArzzezzanStackTest, Constructor_initializer_list_1) {
+  s21::stack<int> s21_stack{1, 2, 4};
+  EXPECT_EQ(s21_stack.size(), 3);
+  EXPECT_EQ(s21_stack.top(), 4);
+}
+
+TEST(ArzzezzanStackTest, Constructor_initializer_list_2) {
+  std::initializer_list<int> b;
+  s21::stack<int> s21_stack{b};
+  std::stack<int> std_stack{b};
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+}
+
+TEST(ArzzezzanStackTest, Constructor_copy_1) {
+  std::initializer_list<int> il1 = {1, 2, 3};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_copy{s21_stack};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_copy{std_stack};
+  EXPECT_EQ(s21_stack_copy.size(), std_stack_copy.size());
+  EXPECT_EQ(s21_stack_copy.top(), std_stack_copy.top());
+}
+
+TEST(ArzzezzanStackTest, Constructor_move_1) {
+  std::initializer_list<int> il1 = {1, 2, 3};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_move{s21_stack};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_move{std_stack};
+  EXPECT_EQ(s21_stack_move.size(), std_stack_move.size());
+  EXPECT_EQ(s21_stack_move.top(), std_stack_move.top());
+}
+
+TEST(ArzzezzanStackTest, Opertator_move_1) {
+  std::initializer_list<int> il1 = {1, 2, 3};
+  std::initializer_list<int> il2 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_move{il2};
+  s21_stack = std::move(s21_stack_move);
+
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_move{il2};
+  std_stack = std::move(std_stack_move);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+}
+
+TEST(ArzzezzanStackTest, Opertator_move_2) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+  std::initializer_list<int> il2 = {1, 2, 3};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_move{il2};
+  s21_stack = std::move(s21_stack_move);
+
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_move{il2};
+  std_stack = std::move(std_stack_move);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+}
+
+TEST(ArzzezzanStackTest, Opertator_move_3) {
+  std::initializer_list<int> il1;
+  std::initializer_list<int> il2 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_move{il2};
+  s21_stack = std::move(s21_stack_move);
+
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_move{il2};
+  std_stack = std::move(std_stack_move);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+}
+
+TEST(ArzzezzanStackTest, Top_1) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+
+  std::stack<int> std_stack{il1};
+
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+}
+
+TEST(ArzzezzanStackTest, Top_2_throw) {
+  std::initializer_list<int> il1;
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+
+  EXPECT_NO_THROW(std_stack.top());
+  EXPECT_NO_THROW(s21_stack.top());
+}
+
+TEST(ArzzezzanStackTest, Empty_1) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+
+  EXPECT_EQ(s21_stack.empty(), std_stack.empty());
+}
+
+TEST(ArzzezzanStackTest, Empty_2) {
+  std::initializer_list<int> il1;
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+
+  EXPECT_EQ(s21_stack.empty(), std_stack.empty());
+}
+
+TEST(ArzzezzanStackTest, Size_1) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+}
+
+TEST(ArzzezzanStackTest, Size_2) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+}
+
+TEST(ArzzezzanStackTest, Push_1) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+  s21_stack.push(1);
+  std_stack.push(1);
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+}
+
+TEST(ArzzezzanStackTest, Push_2) {
+  std::initializer_list<int> il1;
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+  s21_stack.push(1);
+  std_stack.push(1);
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+}
+
+TEST(ArzzezzanStackTest, Pop_1) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+  s21_stack.pop();
+  std_stack.pop();
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  /* EXPECT_EQ(s21_stack.top(), std_stack.top()); */
+}
+
+TEST(ArzzezzanStackTest, Pop_2) {
+  std::initializer_list<int> il1;
+
+  s21::stack<int> s21_stack{il1};
+  std::stack<int> std_stack{il1};
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+}
+
+TEST(ArzzezzanStackTest, Swap_1) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+  std::initializer_list<int> il2 = {1, 2, 3};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_swap{il2};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_swap{il2};
+  s21_stack.swap(s21_stack_swap);
+  std_stack.swap(std_stack_swap);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+
+  EXPECT_EQ(s21_stack_swap.size(), std_stack_swap.size());
+  EXPECT_EQ(s21_stack_swap.top(), std_stack_swap.top());
+}
+
+TEST(ArzzezzanStackTest, Swap_2) {
+  std::initializer_list<int> il1 = {1, 2, 3};
+  std::initializer_list<int> il2 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_swap{il2};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_swap{il2};
+  s21_stack.swap(s21_stack_swap);
+  std_stack.swap(std_stack_swap);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+
+  EXPECT_EQ(s21_stack_swap.size(), std_stack_swap.size());
+  EXPECT_EQ(s21_stack_swap.top(), std_stack_swap.top());
+}
+
+TEST(ArzzezzanStackTest, Swap_3) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+  std::initializer_list<int> il2;
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_swap{il2};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_swap{il2};
+  s21_stack.swap(s21_stack_swap);
+  std_stack.swap(std_stack_swap);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+
+  EXPECT_EQ(s21_stack_swap.size(), std_stack_swap.size());
+}
+
+TEST(ArzzezzanStackTest, Swap_4) {
+  std::initializer_list<int> il1;
+  std::initializer_list<int> il2 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_swap{il2};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_swap{il2};
+  s21_stack.swap(s21_stack_swap);
+  std_stack.swap(std_stack_swap);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+
+  EXPECT_EQ(s21_stack_swap.size(), std_stack_swap.size());
+}
+
+TEST(ArzzezzanStackTest, Swap_5) {
+  std::initializer_list<int> il1;
+  std::initializer_list<int> il2;
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_swap{il2};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_swap{il2};
+  s21_stack.swap(s21_stack_swap);
+  std_stack.swap(std_stack_swap);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+
+  EXPECT_EQ(s21_stack_swap.size(), std_stack_swap.size());
+}
+
+TEST(ArzzezzanStackTest, Swap_6) {
+  std::initializer_list<int> il1 = {4, 5, 6, 7};
+  std::initializer_list<int> il2 = {4, 5, 6, 7};
+
+  s21::stack<int> s21_stack{il1};
+  s21::stack<int> s21_stack_swap{il2};
+  std::stack<int> std_stack{il1};
+  std::stack<int> std_stack_swap{il2};
+  s21_stack.swap(s21_stack_swap);
+  std_stack.swap(std_stack_swap);
+
+  EXPECT_EQ(s21_stack.size(), std_stack.size());
+  EXPECT_EQ(s21_stack.top(), std_stack.top());
+
+  EXPECT_EQ(s21_stack_swap.size(), std_stack_swap.size());
+  EXPECT_EQ(s21_stack_swap.top(), std_stack_swap.top());
+}
+
