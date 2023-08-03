c50bc39d0458077f457b9060bb95a27d0336c07e
joramuns <joramuns@student.21-school.ru>
2023-08-03 16:57:34 +0300

    feat(map): no dynamically allocated tree private member

--- src/map/s21_map.h
+++ src/map/s21_map.h
@@ -26,107 +26,93 @@ class map {
     }
   };
 
-  map(const map &m) {
-    if (rb_tree_ != m.rb_tree_) {
-      clear();
-      delete rb_tree_;
-      rb_tree_ = new tree_type(*m.rb_tree_);
-    }
-  };
+  map(const map &m) : rb_tree_(m.rb_tree_){};
 
-  map(map &&m) {
-    if (rb_tree_ != m.rb_tree_) {
-      std::swap(rb_tree_, m.rb_tree_);
-    }
-  };
+  map(map &&m) : rb_tree_(std::move(m.rb_tree_)){};
 
-  ~map() { delete rb_tree_; };
+  ~map() = default;
 
   map &operator=(const map &m) {
     if (this != &m) {
       clear();
-      delete rb_tree_;
-      rb_tree_ = new tree_type(*m.rb_tree_);
+      rb_tree_ = tree_type(m.rb_tree_);
     }
     return *this;
   };
 
   map &operator=(map &&m) {
     if (this != &m) {
-      *rb_tree_ = std::move(*m.rb_tree_);
+      rb_tree_ = std::move(m.rb_tree_);
     }
     return *this;
   };
 
   /*** Map element access ***/
   mapped_type &at(const Key &key) {
-    iterator iter = rb_tree_->FindKey(key);
+    iterator iter = rb_tree_.FindKey(key);
     return *iter;
   };
 
   mapped_type &operator[](const Key &key) { return at(key); };
 
   /*** Map iterators ***/
-  iterator begin() noexcept { return rb_tree_->begin(); };
+  iterator begin() noexcept { return rb_tree_.begin(); };
 
-  iterator end() noexcept { return rb_tree_->end(); };
+  iterator end() noexcept { return rb_tree_.end(); };
 
-  const_iterator begin() const noexcept { return rb_tree_->begin(); };
+  const_iterator begin() const noexcept { return rb_tree_.begin(); };
 
-  const_iterator end() const noexcept { return rb_tree_->end(); };
+  const_iterator end() const noexcept { return rb_tree_.end(); };
 
   /*** Map capacity ***/
-  bool empty() { return rb_tree_->Empty(); };
+  bool empty() { return rb_tree_.Empty(); };
 
-  size_type size() { return rb_tree_->GetSize(); };
+  size_type size() { return rb_tree_.GetSize(); };
 
-  size_type max_size() { return rb_tree_->MaxSize(); };
+  size_type max_size() { return rb_tree_.MaxSize(); };
 
   /*** Map modifiers ***/
-  void clear() { rb_tree_->ClearTree(); };
+  void clear() { rb_tree_.ClearTree(); };
 
   std::pair<iterator, bool> insert(const value_type &value) {
-    return rb_tree_->InsertNode(value);
+    return rb_tree_.InsertNode(value);
   };
 
   std::pair<iterator, bool> insert(const key_type &key,
                                    const mapped_type &obj) {
-    return rb_tree_->InsertNode((value_type{key, obj}));
+    return rb_tree_.InsertNode((value_type{key, obj}));
   };
 
   std::pair<iterator, bool> insert_or_assign(const key_type &key,
                                              const mapped_type &obj) {
     auto insert_attempt = insert(key, obj);
     if (insert_attempt.second == false) {
-      insert_attempt.first = (iterator)rb_tree_->FindKey(key);
+      insert_attempt.first = (iterator)rb_tree_.FindKey(key);
       *insert_attempt.first = obj;
-      /* insert_attempt.second = true; */
     }
 
     return insert_attempt;
   };
 
-  void erase(iterator pos) { rb_tree_->DeleteNode(pos); };
+  void erase(iterator pos) { rb_tree_.DeleteNode(pos); };
 
   void swap(map &other) {
-    if (rb_tree_ != other.rb_tree_) {
+    if (&rb_tree_ != &other.rb_tree_) {
       std::swap(rb_tree_, other.rb_tree_);
     }
   };
 
   void merge(map &other) {
     if (this != &other) {
-      rb_tree_->Merge(*other.rb_tree_);
+      rb_tree_.Merge(other.rb_tree_);
     }
   };
 
   /*** Map lookup ***/
-  bool contains(const key_type &key) {
-    return rb_tree_->FindKey(key) != end();
-  };
+  bool contains(const key_type &key) { return rb_tree_.FindKey(key) != end(); };
 
  private:
-  tree_type *rb_tree_ = new tree_type;
+  tree_type rb_tree_;
 };
 }  // namespace s21
 
