44551e4d5ef66a052cd1eae9506a8132eb2799f2
joramuns <joramuns@student.21-school.ru>
2023-08-07 20:41:06 +0300

    tests(list): nanadaug emplace

--- src/tests/test_list/test_list_nanadaug.cc
+++ src/tests/test_list/test_list_nanadaug.cc
@@ -1040,56 +1040,56 @@ TYPED_TEST_SUITE(ListSortTest, ListSortTypes);
    EXPECT_EQ(a.size(), 2);
  }
 
-/* TYPED_TEST(ListSortTest, size_10) { */
-/*   using List = typename TestFixture::List; */
+TYPED_TEST(ListSortTest, size_10) {
+  using List = typename TestFixture::List;
 
-/*   List a{}; */
-/*   for (size_t i = 0U; i < 10U; ++i) { */
-/*     if (i % 2U == 0U) { */
-/*       if (i % 3U == 0U) { */
-/*         a.emplace_front(TypeParam(i)); */
-/*       } else { */
-/*         a.emplace_back(TypeParam(i)); */
-/*       } */
-/*     } else { */
-/*       if (i % 3U == 0U) { */
-/*         a.emplace_back(TypeParam(i)); */
-/*       } else { */
-/*         a.emplace_front(TypeParam(i)); */
-/*       } */
-/*     } */
-/*   } */
-/*   a.sort(); */
+  List a{};
+  for (size_t i = 0U; i < 10U; ++i) {
+    if (i % 2U == 0U) {
+      if (i % 3U == 0U) {
+        a.insert_many_front(TypeParam(i));
+      } else {
+        a.insert_many_back(TypeParam(i));
+      }
+    } else {
+      if (i % 3U == 0U) {
+        a.insert_many_back(TypeParam(i));
+      } else {
+        a.insert_many_front(TypeParam(i));
+      }
+    }
+  }
+  a.sort();
 
-/*   size_t i = 0U; */
-/*   for (auto iterator = a.begin(); iterator != a.end(); ++iterator) { */
-/*     EXPECT_DOUBLE_EQ(iterator.head->value, TypeParam(i++)); */
-/*   } */
-/* } */
+  size_t i = 0U;
+  for (auto it = a.begin(); it != a.end(); ++it) {
+    EXPECT_DOUBLE_EQ(*it, TypeParam(i++));
+  }
+}
 
-/* TYPED_TEST(ListSortTest, size_100) { */
-/*   using List = typename TestFixture::List; */
+TYPED_TEST(ListSortTest, size_100) {
+  using List = typename TestFixture::List;
 
-/*   List a{}; */
-/*   for (size_t i = 0U; i < 100U; ++i) { */
-/*     if (i % 2U == 0U) { */
-/*       if (i % 3U == 0U) { */
-/*         a.emplace_front(TypeParam(i)); */
-/*       } else { */
-/*         a.emplace_back(TypeParam(i)); */
-/*       } */
-/*     } else { */
-/*       if (i % 3U == 0U) { */
-/*         a.emplace_back(TypeParam(i)); */
-/*       } else { */
-/*         a.emplace_front(TypeParam(i)); */
-/*       } */
-/*     } */
-/*   } */
-/*   a.sort(); */
+  List a{};
+  for (size_t i = 0U; i < 100U; ++i) {
+    if (i % 2U == 0U) {
+      if (i % 3U == 0U) {
+        a.insert_many_front(TypeParam(i));
+      } else {
+        a.insert_many_back(TypeParam(i));
+      }
+    } else {
+      if (i % 3U == 0U) {
+        a.insert_many_back(TypeParam(i));
+      } else {
+        a.insert_many_front(TypeParam(i));
+      }
+    }
+  }
+  a.sort();
 
-/*   size_t i = 0U; */
-/*   for (auto iterator = a.begin(); iterator != a.end(); ++iterator) { */
-/*     EXPECT_DOUBLE_EQ(iterator.head->value, TypeParam(i++)); */
-/*   } */
-/* } */
+  size_t i = 0U;
+  for (auto it = a.begin(); it != a.end(); ++it) {
+    EXPECT_DOUBLE_EQ(*it, TypeParam(i++));
+  }
+}
