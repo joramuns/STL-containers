07f58fa3cbb8bd0638c385db88ac73072cc809a4
joramuns <joramuns@student.21-school.ru>
2023-08-07 18:59:21 +0300

    tests(array): common test header

--- src/tests/tests.h
+++ src/tests/tests.h
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
 
 #include <array>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
 
 #include "../s21_containers.h"
 #include "../s21_containersplus.h"
