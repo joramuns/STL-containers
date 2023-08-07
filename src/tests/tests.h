863d2db29881e7101e0b6f1967c316a899cc4e13
joramuns <joramuns@student.21-school.ru>
2023-08-07 18:59:40 +0300

    tests(map): common header

--- src/tests/tests.h
+++ src/tests/tests.h
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <array>
+#include <list>
 #include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
 
 #include "../s21_containers.h"
